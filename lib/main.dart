bc82fe49e9ec8a8b422e8423ac75a8b8f0d4e39e
Mian Computer <amirhameed012@gmail.com>
2020-09-25 14:12:20 +0500

    dfbbdfbfg

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:keyboard_visibility/keyboard_visibility.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,109 +10,392 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: LoginPage(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class LoginPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginPageState extends State<LoginPage> {
+  int _pageState = 0;
+  var _backgroundColor = Colors.white;
+  var _headingColor = Colors.white;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  double _heading_top = 100;
+  double loginHeight = 0;
+  double registerHeight = 0;
+
+  double loginOpacity = 1;
+  double loginWidth = 0;
+  double loginYoffset = 0;
+  double loginXoffset = 0;
+  double registerYoffset = 0;
+
+  double windowWidth = 0;
+  double windowHeight = 0;
+  bool _keyboardVisilble = false;
+  @override
+  void initState() {
+    super.initState();
+    KeyboardVisibilityNotification().addNewListener(
+      onChange: (bool visible) {
+        setState(() {
+          _keyboardVisilble = visible;
+        });
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    windowHeight = MediaQuery.of(context).size.height;
+    windowWidth = MediaQuery.of(context).size.width;
+    loginHeight = windowHeight - 270;
+    registerHeight = windowHeight - 270;
+    switch (_pageState) {
+      case 0:
+        _backgroundColor = Colors.white;
+        _headingColor = Colors.purple;
+        loginYoffset = windowHeight;
+        loginXoffset = 0;
+        loginOpacity = 1;
+        _heading_top = 100;
+        loginWidth = windowWidth;
+        loginHeight = _keyboardVisilble ? windowHeight : windowHeight - 270;
+        registerYoffset = windowHeight;
+        break;
+      case 1:
+        _backgroundColor = Colors.pink;
+        _headingColor = Colors.white;
+        loginYoffset = _keyboardVisilble ? 40 : 270;
+        loginHeight = _keyboardVisilble ? windowHeight : windowHeight - 270;
+        loginXoffset = 0;
+        _heading_top = 90;
+        loginOpacity = 1;
+        loginWidth = windowWidth;
+        registerYoffset = windowHeight;
+        break;
+      case 2:
+        _backgroundColor = Colors.pink;
+        _headingColor = Colors.white;
+        loginYoffset = 240;
+        loginXoffset = 20;
+        _heading_top = 80;
+        loginOpacity = 0.7;
+        loginYoffset = _keyboardVisilble ? 30 : 240;
+        loginHeight = _keyboardVisilble ? windowHeight : windowHeight - 240;
+        loginWidth = windowWidth - 40;
+        registerYoffset = _keyboardVisilble ? 55 : 270;
+        registerHeight = _keyboardVisilble ? windowHeight : windowHeight - 240;
+        break;
+    }
+
+    return SingleChildScrollView(
+      child: Stack(
+        children: [
+          AnimatedContainer(
+            curve: Curves.fastLinearToSlowEaseIn,
+            duration: Duration(microseconds: 1000),
+            color: _backgroundColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _pageState = 0;
+                    });
+                  },
+                  child: Container(
+                    child: Column(
+                      children: [
+                        AnimatedContainer(
+                            curve: Curves.fastLinearToSlowEaseIn,
+                            duration: Duration(milliseconds: 1000),
+                            margin: EdgeInsets.only(top: _heading_top),
+                            child: Text(
+                              'Learn Free',
+                              style:
+                              TextStyle(color: _headingColor, fontSize: 28),
+                            )),
+                        Container(
+                          margin: EdgeInsets.all(20),
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(
+                            'We make learning easy.Androi studio to learn flutter for bigner and advance level student.',
+                            style: TextStyle(
+                              color: _headingColor,
+                              fontSize: 16,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 32),
+                  child: Center(
+                    child: Image.asset('assets/images/splash_bg.png'),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(28),
+                  padding: EdgeInsets.all(20),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (_pageState != 0) {
+                          _pageState = 0;
+                        } else {
+                          _pageState = 1;
+                        }
+                      });
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          color: Colors.purple,
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Center(
+                        child: Text(
+                          'Get Started',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          AnimatedContainer(
+            padding: EdgeInsets.all(32),
+            width: loginWidth,
+            height: loginHeight,
+            duration: Duration(milliseconds: 1000),
+            curve: Curves.fastLinearToSlowEaseIn,
+            transform: Matrix4.translationValues(loginXoffset, loginYoffset, 1),
+            decoration: BoxDecoration(
+                color: Colors.white.withOpacity(loginOpacity),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      child: Text(
+                        'Login To Continue',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    InputWithIcon(
+                      icon: Icons.email,
+                      hint: 'Enter Email ....',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InputWithIcon(
+                      icon: Icons.vpn_key,
+                      hint: 'Enter Password ....',
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    PrimaryButton(
+                      btnText: 'Login',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _pageState = 2;
+                        });
+                      },
+                      child: OutlinceButton(
+                        btnText: 'Create New Account',
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          AnimatedContainer(
+            height: registerHeight,
+            padding: EdgeInsets.all(32),
+            duration: Duration(milliseconds: 1000),
+            curve: Curves.fastLinearToSlowEaseIn,
+            transform: Matrix4.translationValues(0, registerYoffset, 1),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      child: Text(
+                        'Create a New Account',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    InputWithIcon(
+                      icon: Icons.email,
+                      hint: 'Enter Email ....',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InputWithIcon(
+                      icon: Icons.vpn_key,
+                      hint: 'Enter Password ....',
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    PrimaryButton(
+                      btnText: 'Create Account',
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _pageState = 1;
+                        });
+                      },
+                      child: OutlinceButton(
+                        btnText: 'Back To Login',
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+}
+
+class PrimaryButton extends StatefulWidget {
+  final String btnText;
+  PrimaryButton({this.btnText});
+  @override
+  _PrimaryButtonState createState() => _PrimaryButtonState();
+}
+
+class _PrimaryButtonState extends State<PrimaryButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.purple, borderRadius: BorderRadius.circular(50)),
+      padding: EdgeInsets.all(20),
+      child: (Center(
+        child: Text(
+          widget.btnText,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
         ),
+      )),
+    );
+  }
+}
+
+class InputWithIcon extends StatefulWidget {
+  final IconData icon;
+  final String hint;
+  InputWithIcon({this.icon, this.hint});
+
+  @override
+  _InputWithIconState createState() => _InputWithIconState();
+}
+
+class _InputWithIconState extends State<InputWithIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.purple, width: 2),
+          borderRadius: BorderRadius.circular(50)),
+      child: Row(
+        children: [
+          Container(
+              width: 60,
+              child: Icon(
+                widget.icon,
+                color: Colors.grey,
+                size: 20,
+              )),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: widget.hint,
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(vertical: 20)),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class OutlinceButton extends StatefulWidget {
+  final String btnText;
+  OutlinceButton({this.btnText});
+  @override
+  _OutlinceButtonState createState() => _OutlinceButtonState();
+}
+
+class _OutlinceButtonState extends State<OutlinceButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.purple, width: 2),
+          borderRadius: BorderRadius.circular(50)),
+      padding: EdgeInsets.all(20),
+      child: (Center(
+        child: Text(
+          widget.btnText,
+          style: TextStyle(
+            color: Colors.purple,
+            fontSize: 16,
+          ),
+        ),
+      )),
     );
   }
 }
